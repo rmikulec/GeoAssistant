286d5e5954656d7e04ed7f82dd6599f6173924f0
Ryan Mikulec <ryan.mikulec@mikulec.net>
2025-06-17 12:17:26 -0400

    Added doc strings to data handler

--- geo_assistant/handlers/_data_handler.py
+++ geo_assistant/handlers/_data_handler.py
@@ -4,12 +4,25 @@ from sqlalchemy import create_engine, text
 from geo_assistant.handlers._filter import GeoFilter
 
 class DataHandler:
+    """
+    A small handler class to interact directly with the PostGIS Database
+    """
     
     def __init__(self, db_name: str, table_name: str):
         self.engine = create_engine(f"postgresql+psycopg2://gisuser:pw@localhost:5432/{db_name}")
         self.table_name = table_name
     
-    def get_geojson(self, lat: float, long: float):
+    def get_geojson(self, lat: float, long: float) -> gpd.GeoDataFrame:
+        """
+        Gets the geo json data based on a given lat / long
+
+        Args:
+            - lat(float); The latitude of the expected entry
+            - long(float): The longitude of the expected entry
+        
+        Returns:
+            (gpd.GeoDataFrame): A dataframe of all entries that contains the (lat/long)
+        """
         sql = f"""
         SELECT *
         FROM {self.table_name}
@@ -29,7 +42,15 @@ class DataHandler:
         return gdf_match
 
 
-    def filter_count(self, filters: list[GeoFilter]):
+    def filter_count(self, filters: list[GeoFilter]) -> int:
+        """
+        Returns the count of entries in the database that adhere to the given filters
+
+        Args:
+            filters(list[GeoFilter]): The filters that the user is querying on
+        Returns:
+            int: The number of entries in the database that adhere to the filters
+        """
         sql = f"""
         SELECT count(*)
         FROM {self.table_name}
