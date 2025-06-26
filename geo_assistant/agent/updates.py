92e8f1d14864c286db9c1138e2970ba450362efc
Ryan Mikulec <ryan.mikulec@mikulec.net>
2025-06-26 10:14:38 -0400

    Added new agent code, note this was pulled from a previous project

---
+++ geo_assistant/agent/updates.py
@@ -0,0 +1,30 @@
+from pydantic import BaseModel
+from typing import Literal
+from enum import Enum
+
+
+class Status(Enum):
+    GENERATING = "generating"
+    PROCESSING = "processing"
+    SUCCEDED = "succeded"
+    ERROR = "error"
+
+
+class EmitUpdate(BaseModel):
+    type: Literal['base'] = 'base'
+    status: Status
+
+
+class AiUpdate(EmitUpdate):
+    type: Literal['ai_response'] = 'ai_response'
+    message: str = None
+    tool_call: str = None
+    tool_args: dict = None
+
+
+class AnalysisUpdate(EmitUpdate):
+    type: Literal['analysis'] = 'analysis'
+    id: str
+    query: str
+    step: str
+    progress: float
