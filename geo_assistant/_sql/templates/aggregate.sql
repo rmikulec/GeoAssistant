ccc30dc90310a8b628e87daabeb0cf872779e9c5
Ryan Mikulec <ryan.mikulec@mikulec.net>
2025-06-26 21:07:48 -0400

    Fixed up aggregate

--- geo_assistant/_sql/templates/aggregate.sql
+++ geo_assistant/_sql/templates/aggregate.sql
@@ -2,65 +2,55 @@
 DROP TABLE IF EXISTS "{{ schema }}"."{{ output_table }}";
 
 CREATE TABLE "{{ schema }}"."{{ output_table }}" AS
-SELECT
-  {%- set aggs = aggregators | default([]) -%}
-  {%- for agg in aggs %}
-    {%- if agg.operator == 'COUNT' %}
-      COUNT(
-        {{ 'DISTINCT ' if agg.distinct else '' -}}
-        {%- if agg.column.value != '*' -%}
-          "{{ agg.column.value }}"
-        {%- else -%}
-          *
-        {%- endif -%}
-      )
-    {%- else %}
-      {{ agg.operator }}("{{ agg.column.value }}")
-    {%- endif %}
-    AS
-      "{{ (agg.alias
-           | default(agg.operator|lower ~ '_' ~ 
-             (agg.column.value == '*' and 'all' or agg.column.value)
-           )
-         ) }}"
-    {%- if not loop.last %},{% endif %}
-  {%- endfor %}
-  {%- if aggs|length > 0 %},{% endif %}
+WITH src AS (
+  SELECT
+    {% for col in group_by %}
+      "{{ col.value }}",
+    {% endfor %}
+    {% for agg in select if agg.column.value != '*' %}
+      "{{ agg.column.value }}",
+    {% endfor %}
+    ST_Transform("{{ geometry_column }}", {{ srid }})::Geometry({{ gtype }}, {{ srid }}) AS "geom{{ srid }}"
+  FROM "{{ source_table.source_schema }}"."{{ source_table.source_table }}"
+)
 
-  -- spatial aggregation ↓
-  ST_SetSRID(
-    {%- if spatial_aggregator %}
-      {%- set spa = spatial_aggregator | upper -%}
-      {%- if spa == 'COLLECT' -%}
-        ST_Collect("{{ geometry_column }}")
-      {%- elif spa == 'UNION' -%}
-        ST_Union("{{ geometry_column }}")
-      {%- elif spa == 'CENTROID' -%}
-        ST_Centroid("{{ geometry_column }}")
-      {%- elif spa == 'ENVELOPE' -%}
-        ST_Envelope("{{ geometry_column }}")
-      {%- elif spa == 'CONVEXHULL' -%}
-        ST_ConvexHull("{{ geometry_column }}")
-      {%- elif spa == 'EXTENT' -%}
-        -- convert the BOX2D into a polygon
-        ST_ConvexHull(ST_Envelope("{{ geometry_column }}"))
-      {%- else -%}
-        "{{ geometry_column }}"
-      {%- endif -%}
-    {%- else -%}
-      "{{ geometry_column }}"
-    {%- endif -%}
-    , {{ srid | default(4326) }}
-  )::Geometry(
-    {{ gtype | default('MultiLineString') }},
-    {{ srid | default(4326) }}
-  ) AS "{{ geometry_column }}"
-FROM
-  "{{ source_table.source_schema | default('public') }}"."{{ source_table.source_table | default('input') }}"
-{%- if group_by %}
+SELECT
+  {% for col in group_by %}
+    src."{{ col.value }}",
+  {% endfor %}
+  {% for agg in select %}
+    {% if agg.operator == 'COUNT' %}
+      COUNT({{ 'DISTINCT ' if agg.distinct else '' }}{{ '"{}"'.format(agg.column.value) if agg.column.value != '*' else '*' }}) AS "{{ agg.alias | default('count_' ~ agg.column.value) }}",
+    {% else %}
+      {{ agg.operator }}("{{ agg.column.value }}") AS "{{ agg.alias | default(agg.operator | lower ~ '_' ~ agg.column.value) }}",
+    {% endif %}
+  {% endfor %}
+  {# Geometry Aggregation #}
+  {% set spa = (spatial_aggregator or 'CENTROID') | upper %}
+  {% if spa == 'COLLECT' %}
+    ST_Collect("geom{{ srid }}") AS "geom{{ srid }}"
+  {% elif spa == 'UNION' %}
+    ST_Union("geom{{ srid }}") AS "geom{{ srid }}"
+  {% elif spa == 'CENTROID' %}
+    ST_Centroid(ST_Collect("geom{{ srid }}")) AS "geom{{ srid }}"
+  {% elif spa == 'ENVELOPE' %}
+    ST_Envelope(ST_Collect("geom{{ srid }}")) AS "geom{{ srid }}"
+  {% elif spa == 'CONVEXHULL' %}
+    ST_ConvexHull(ST_Union("geom{{ srid }}")) AS "geom{{ srid }}"
+  {% elif spa == 'EXTENT' %}
+    ST_ConvexHull(ST_Envelope(ST_Union("geom{{ srid }}"))) AS "geom{{ srid }}"
+  {% endif %}
+
+FROM src
 GROUP BY
-  {%- for col in group_by %}
-    "{{ col.value }}"{{ "," if not loop.last else "" }}
-  {%- endfor %}
-  {%- if spatial_aggregator %}, "{{ geometry_column }}"{% endif %}
-{%- endif %};
+  {% for col in group_by %}
+    "{{ col.value }}"{% if not loop.last %},{% endif %}
+  {% endfor %};
+
+-- Drop the original geometry column
+ALTER TABLE "{{ schema }}"."{{ output_table }}"
+DROP COLUMN IF EXISTS "{{ geometry_column }}";
+
+-- Rename the aggregated geometry column
+ALTER TABLE "{{ schema }}"."{{ output_table }}"
+RENAME COLUMN "geom{{ srid }}" TO "{{ geometry_column }}";
